60d48f15bd6d56ce1d9ce6e8de8794482c0ce8a1
jirawat malaiwan <tawan.jirawat@gmail.com>
2023-07-20 15:41:11 +0700

    1

--- Lab/Lab04-Variable-Operator.cpp
+++ Lab/Lab04-Variable-Operator.cpp
@@ -32,11 +32,11 @@ int main() {
     float time; 
     float money;
 
-    printf( "Input the Employees ID(Max. 10 chars)\n" );
+    printf( "Input the Employees ID(Max. 10 chars) :\n" );
     scanf( "%s" , &ID );
-    printf( "Input the working hrs\n" );
+    printf( "Input the working hrs :\n" );
     scanf( "%f" , &time );
-    printf( "Salary amount/hr:\n" );
+    printf( "Salary amount/hr :\n" );
     scanf( "%f" , &money );
 
     total = time*money ;
